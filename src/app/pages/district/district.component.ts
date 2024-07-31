2b645e2b3a88a53d594a814ed80a792a9cc84855
project@2004 <antonyvarghese057@gmail.com>
2024-07-31 15:44:33 +0530

    first commit

---
+++ src/app/pages/district/district.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-district',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './district.component.html',
+  styleUrl: './district.component.css'
+})
+export class DistrictComponent {
+  products=[
+  
+  { 
+     id	:1,
+     title	:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+     price:	109.95,
+     description	:"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+     category:	"men's clothing",
+     image	:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+     rate	:3.9,
+     count	:120
+  },
+  { 
+    id	:1,
+    title	:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price:	109.95,
+    description	:"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category:	"men's clothing",
+    image	:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rate	:3.9,
+    count	:120
+ }
+  ]
+
+}
